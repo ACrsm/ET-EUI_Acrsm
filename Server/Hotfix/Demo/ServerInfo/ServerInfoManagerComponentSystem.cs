5d3eab6f7f80d1e3e67dcca892d6989557ff6b11
ACrsm <acrsm123@gmail.com>
2024-08-17 17:53:22 +0800

    23 课前面 修改ServerInfo

--- Server/Hotfix/Demo/ServerInfo/ServerInfoManagerComponentSystem.cs
+++ Server/Hotfix/Demo/ServerInfo/ServerInfoManagerComponentSystem.cs
@@ -30,6 +30,7 @@ namespace ET
     }
     
     [FriendClass(typeof(ServerInfoManagerComponent))]
+    [FriendClass(typeof(ServerInfo))]
     public static class ServerInfoManagerComponentSystem
     {
         public static async ETTask Awake(this ServerInfoManagerComponent self)
@@ -37,12 +38,24 @@ namespace ET
             var serverInfoList = await DBManagerComponent.Instance.GetZoneDB(self.DomainZone()).Query<ServerInfo>(d => true);
             if (serverInfoList == null || serverInfoList.Count <= 0)
             {
-                Log.Info("Server Info Count Zero");
+                Log.Info("Query DB Server Info Count Zero");
+                
+                self.ServerInfos.Clear();
+                var serverInfoConfigs = ServerInfoConfigCategory.Instance.GetAll();
+                foreach (var info in serverInfoConfigs.Values)
+                {
+                    ServerInfo newServerInfo = self.AddChildWithId<ServerInfo>(info.Id);
+                    newServerInfo.ServerName = info.ServerName;
+                    newServerInfo.Status = (int)ServerStatus.Normal;
+                    self.ServerInfos.Add(newServerInfo);
+                    await DBManagerComponent.Instance.GetZoneDB(self.DomainZone()).Save(newServerInfo);
+                }
+                
                 return; 
             }
             
             self.ServerInfos.Clear();
-            foreach (var serverInfo in self.ServerInfos)
+            foreach (var serverInfo in serverInfoList)
             {
                 self.AddChild(serverInfo);
                 self.ServerInfos.Add(serverInfo);
